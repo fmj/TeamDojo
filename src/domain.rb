45266c3a2c0b86a4703fe8bf15a43d97eea71929
Karianne Berg <karianne.berg@gmail.com>
2013-03-15 11:04:48 +0100

    initial distance calculations

--- src/domain.rb
+++ src/domain.rb
@@ -1,8 +1,29 @@
+require './graphs.rb'
 
 class Programmer
-  def initialize(name, skills)
+  def initialize(name, skills = [])
     @name = name
     @skills = skills
   end
   
 end
+
+class Network
+
+	def initialize
+		@graph = Graph.new
+	end
+
+	def add(programmer)
+		@graph.push(programmer)
+	end
+
+	def recommendation(recommender, recommendee)
+		@graph.connect(recommender, recommendee)
+	end
+
+	def distance_between(one, two)
+		@graph.dijkstra(one, two)
+	end
+end
+
