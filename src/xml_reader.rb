4f580b5db82c351874046376541bc0e14994b6a8
Kristian Andre Kastet <kakastet@kakmba.local>
2013-03-15 10:46:38 +0100

    latest xml reader

--- src/xml_reader.rb
+++ src/xml_reader.rb
@@ -1,6 +1,9 @@
 require "Nokogiri"
 
 f = File.open("ProNet.xml")
-doc = Nokogiri::XML(f)
+@doc = Nokogiri::XML(f)
+
+puts doc.xpath"/*[local-name()='Network']/*[local-name()='Programmer']"
+
 f.close
 
