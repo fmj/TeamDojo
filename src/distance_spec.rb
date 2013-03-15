b89ad5c7ad12259522c5caba2fbc0247cdcd6e11
Karianne Berg <karianne.berg@gmail.com>
2013-03-15 11:12:51 +0100

    refactored to use constants

--- src/distance_spec.rb
+++ src/distance_spec.rb
@@ -5,24 +5,25 @@ describe @network, "#distance_between" do
 
   before :each do
     @network = Network.new
+
+    @dave = Programmer.new("Dave")
+    @bob = Programmer.new("Bob")
   end
 
   it "returns 0 for same person" do
-    dave = Programmer.new("Dave")
-    @network.add(dave)
-    @network.distance_between(dave, dave).should eq 0
+    @network.add(@dave)
+    @network.distance_between(@dave, @dave).should eq 0
 
   end
 
   it "returns 1 for neighbour" do
-    dave = Programmer.new("Dave")
-    bob = Programmer.new("Bob")
 
-    @network.add(dave)
-    @network.add(bob)
 
-    @network.recommendation(dave, bob)
+    @network.add(@dave)
+    @network.add(@bob)
 
-    @network.distance_between(dave, bob).should eq 1
+    @network.recommendation(@dave, @bob)
+
+    @network.distance_between(@dave, @bob).should eq 1
   end
 end
