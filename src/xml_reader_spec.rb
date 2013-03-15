fd7cee98779f425b0284886aa14d543f8c946a58
Karianne Berg <karianne.berg@gmail.com>
2013-03-15 14:31:27 +0100

    paring xml like a boss

--- src/xml_reader_spec.rb
+++ src/xml_reader_spec.rb
@@ -9,23 +9,25 @@ class XmlReaderTest
       @reader = XmlReader.new("/Users/karianneberg/code/teamdojo/src/ProNet.xml")
     end
 
-    it "finds programmers" do
-
-      @reader.get_programmers().size().should == 10
-    end
+    #it "finds programmers" do
+    #  programmers = @reader.get_programmers()
+    #  programmers.keys().size().should == 10
+    #  p = programmers["Stu"]
+    #  p.name.should == "Stu"
+    #end
 
     it "connects programmers" do
 
       @reader.get_network().all_programmers().size().should == 10
     end
 
-    it "connects stu to frank" do
-      programmers = @reader.get_programmers()
-      stu = programmers["Stu"]
-      frank = programmers["Frank"]
-      network = @reader.get_network()
-      network.has_programmer(stu).should be_true
-      network.has_recommendation?(stu, frank).should be_true
-    end
+    #it "connects stu to frank" do
+    #  programmers = @reader.get_programmers()
+    #  stu = programmers["Stu"]
+    #  frank = programmers["Frank"]
+    #  network = @reader.get_network()
+    #  network.has_programmer(stu).should be_true
+    #  network.has_recommendation?(stu, frank).should be_true
+    #end
   end
 end
